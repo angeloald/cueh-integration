a343de831c410c9bf83c481a24ebe30256a3c849
Angelo <angelokn14@gmail.com>
2020-03-27 01:44:00 -0700

    feat: basic home page that shows req, res works

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
-module.exports = {};
+const express = require("express");
+const router = express.Router();
+
+const { getHome } = require("../controllers");
+
+router.get("/", getHome);
+
+module.exports = router;
