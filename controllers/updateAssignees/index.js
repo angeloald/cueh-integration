d8016e1884517dc70e8cb920b31da09bb0c658bf
Angelo <angelokn14@gmail.com>
2020-03-31 13:12:05 -0700

    feat: added a standard delay prior to queueing tasks

--- controllers/updateAssignees/index.js
+++ controllers/updateAssignees/index.js
@@ -1,8 +1,11 @@
 const { fnMap } = require("../../services/everhour");
 const { exists, enqueue } = require("../../services/syncQueue");
+const { delay } = require("../../utils");
 
 module.exports = async (req, res) => {
   res.sendStatus(200);
+
+  await delay(process.env.STANDARD_DELAY || 0);
 
   const fnData = {
     clickupTaskId: req.body.task_id,
