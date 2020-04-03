b76b1d8a2c0ba1eff3527ec63921408152d5f5fc
Angelo <angelokn14@gmail.com>
2020-04-02 18:55:40 -0700

    feat: added HMAC validation and combined routes into one

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getHome,
-  deleteTask,
-  updateEstimate,
-  updateDueDate,
-  updateAssignees,
-  createTask
-} = require("../controllers");
+const crypto = require("crypto");
+const key = process.env.WEBHOOKS_SECRET;
 
-router.get("/", getHome);
-router.post("/deleteTask", deleteTask);
-router.post("/updateEstimate", updateEstimate);
-router.post("/updateDueDate", updateDueDate);
-router.post("/updateAssignees", updateAssignees);
-router.post("/createTask", createTask);
+const { routesCtl } = require("../controllers");
+
+// This ensures that ClickUp is the only that's able to send us data
+router.use((req, res, next) => {
+  if (key === undefined) {
+    console.error(
+      "Can't propagate request. Your webhooks secret is undefined. Set your WEBHOOKS_SECRET environment variable first"
+    );
+    return res.sendStatus(200);
+  }
+  const body = JSON.stringify(req.body);
+  const hash = crypto.createHmac("sha256", key).update(body);
+  const signature = hash.digest("hex");
+  const xSignature = req.headers["x-signature"];
+  if (signature !== xSignature) {
+    console.error(
+      `Can't propagate request. HMAC validation failure for ${req.body.task_id}`
+    );
+    return res.sendStatus(403);
+  }
+  next();
+});
+
+router.post("/", routesCtl);
 
 module.exports = router;
